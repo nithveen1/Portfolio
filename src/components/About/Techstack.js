9203526b4d186f11724d402a034582ad2ff353ff
nithveen1 <107979233+nithveen1@users.noreply.github.com>
2023-01-14 12:44:56 +0530

    Add files via upload

---
+++ src/components/About/Techstack.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Col, Row } from "react-bootstrap";
+import {
+  DiJavascript1,
+  DiReact,
+  DiPython,
+} from "react-icons/di";
+
+function Techstack() {
+  return (
+    <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
+     
+      <Col xs={4} md={2} className="tech-icons">
+        <DiJavascript1 />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiReact />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiPython />
+      </Col>
+    </Row>
+  );
+}
+
+export default Techstack;
