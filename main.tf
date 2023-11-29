db1c4b084fd8eb8ecad0c58c86231bb1ddbaa769
nithveen1 <107979233+nithveen1@users.noreply.github.com>
2023-11-29 18:36:54 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,16 @@
 provider "aws" {
   region = "us-east-1"  
+  access_key = var.access
+  secret_key = var.secreate
 }
 
+variable "access" {
+  type = string
+}
+
+variable "secreate" {
+  type = string
+}
 resource "aws_instance" "my_server"  {
   ami           = "ami-0fc5d935ebf8bc3bc"
   instance_type = "t2.micro"  
@@ -11,3 +20,5 @@ resource "aws_instance" "my_server"  {
     Name = "my_server_Example"
   }
 }
+
+
